2b697d50a2db1ced08a64df5031a2df5aca1263e
Praymore Omeni <omenipraymore18@gmail.com>
2024-10-22 04:42:07 +0000

    Assign variables to DOM elements and use event listeners to toggle between #book-details two classes

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let closeFormBtn = document.querySelector(".close-form");
+let background = document.querySelector('.background');
+let bookDetails = document.querySelector("#book-details");
+let addBook = document.querySelector("#add-button");
+
+window.addEventListener("load", function(){
+  bookDetails.classList.remove("book-details")
+})
+
+addBook.addEventListener("click", function(){
+  bookDetails.classList.add("book-details");
+  background.style.filter = 'blur(5px)';
+
+});
+
+
+closeFormBtn.addEventListener("click", function() {
+  bookDetails.classList.remove("book-details"); 
+  background.style.filter = 'blur(0px)';
+});
+
+
 const myLibrary = [];
 
 function Book() {
